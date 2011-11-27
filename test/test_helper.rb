9da9ae2267ec5d8d4a3d694b54f57cd6524eb185
kimoto <sub+peerler@gmail.com>
2011-11-27 22:47:12 +0900

    Initial commit to proc-cache.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'proc-cache'
+
+class Test::Unit::TestCase
+end
